6af5cddd19d15a7ba80a756925cb25c738eb88ee
ebtihalaziz <80175262+ebtihalaziz@users.noreply.github.com>
2021-04-18 00:52:44 +0300

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -27,7 +27,7 @@ SEGMENTED_MODELS = [
 ]
 
 
-class ArbertmoPreprocessor:
+class ArabertPreprocessor:
     """
     A Preprocessor class that cleans and preprocesses text for all models in the AraBERT repo.
     It also can unprocess the text ouput of the generated text
